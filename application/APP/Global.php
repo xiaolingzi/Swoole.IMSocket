1f865200afe689e5a6b683d3036252a96f7036c8
zhouyongling <yonglingzhou@imscv.com>
2017-09-04 14:43:43 +0800

    global variable edit

--- application/APP/Global.php
+++ application/APP/Global.php
@@ -1,7 +1,7 @@
 <?php
 //项目根目录
-define("ROOT_PATH", getcwd());
+define("ROOT_PATH", dirname(realpath($_SERVER['SCRIPT_FILENAME'])));
 //框架目录
-define("FRAME_PATH", dirname(dirname(getcwd())));
+define("FRAME_PATH", dirname(dirname(__FILE__)));
 //环境变量
 define("ENVIRONMENT", "dev");
